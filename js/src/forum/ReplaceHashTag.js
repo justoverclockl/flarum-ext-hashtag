b1d8222ab8930a82db1e9afe03773077b95c533e
Billy Wilcosky <41351317+zerosonesfun@users.noreply.github.com>
2021-10-25 22:45:29 -0400

    Update ReplaceHashTag.js
    Support non-spacing marks (i.e. ◌ۜ)

--- js/src/forum/ReplaceHashTag.js
+++ js/src/forum/ReplaceHashTag.js
@@ -1,7 +1,7 @@
 import app from 'flarum/forum/app';
 
 export default function () {
-  const regex = /(?<=\s|^)#(\w*[A-Za-z_]+\w*)/g;
+  const regex = /(^|\s)#(\p{L}*\p{S}*\p{M}*)/gu;
   const p = this.$('p');
   const discussionsUrl = app.route('index');
   const tooltip = app.translator.trans('justoverclock-hashtag.forum.post.hashtag_link_tooltip');
@@ -12,7 +12,7 @@ export default function () {
     $(element).html(
       $(element)
         .html()
-        .replace(regex, (match) => `<a href="${discussionsUrl}?q=${match.slice(1)}" class="hasht" title="${tooltip}">${match}</a>`)
+        .replace(regex, (match) => ` <a href="${discussionsUrl}?q=${match.replace(/#/g,'').trim()}" class="hasht" title="${tooltip}">${match.trim()}</a>`)
     );
     setTimeout(() => {
       $(element)
