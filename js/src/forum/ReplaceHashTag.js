31f9d3530c3784901e445153b0baf88f3a303f56
Marco Colia <info@justoverclock.it>
2022-03-03 08:47:57 +0100

    fixes to hashtag ext

--- js/src/forum/ReplaceHashTag.js
+++ js/src/forum/ReplaceHashTag.js
@@ -1,12 +1,14 @@
 import app from 'flarum/forum/app';
 
 export default function () {
-  if (app.forum.attribute('justoverclock-hashtag.regex')) {
-  const regex = new RegExp('' + app.forum.attribute('justoverclock-hashtag.regex'), 'gu');
+
+  const regex = /(^|\s)#(\p{L}+)/gu;
   const p = this.$('p');
   const discussionsUrl = app.route('index');
   const tooltip = app.translator.trans('justoverclock-hashtag.forum.post.hashtag_link_tooltip');
 
+  // rimuoviamo il carattere # utilizzando match.slice nel link
+  // thanks to Nearata for this fix
   p.each((index, element) => {
     $(element).html(
       $(element)
@@ -23,28 +25,4 @@ export default function () {
         });
     }, 1);
   });
-}
-if (app.forum.attribute('justoverclock-hashtag.regex') === '') {
-  const regex = /(^|\s)#(\p{L}*\p{S}*\p{M}*)/gu;
-  const p = this.$('p');
-  const discussionsUrl = app.route('index');
-  const tooltip = app.translator.trans('justoverclock-hashtag.forum.post.hashtag_link_tooltip');
-
-  p.each((index, element) => {
-    $(element).html(
-      $(element)
-        .html()
-        .replace(regex, (match) => ` <a href="${discussionsUrl}?q=${match.replace(/#/g,'').trim()}" class="hasht" title="${tooltip}">${match.trim()}</a>`)
-    );
-    setTimeout(() => {
-      $(element)
-        .find('a.hasht')
-        .click((e) => {
-          if (e.ctrlKey || e.metaKey || e.which === 2) return;
-          e.preventDefault();
-          m.route.set(e.target.href);
-        });
-    }, 1);
-  });
-}
 }
